a53b710594245e4edb8f2e976dcffc7dcb3c2279
Hart Traveller <hart@MBP.local>
2023-06-19 21:28:31 -0400

    ciawf/web: make cache private

--- ciawf/web.py
+++ ciawf/web.py
@@ -1,7 +1,7 @@
 class Interface:
     def __init__(self, cache_refresh_period_seconds: int = 86_400) -> None:
         self.__crps = cache_refresh_period_seconds
-        self.cache: dict = dict()
+        self.__cache: dict = dict()
 
     def get_state_names(self):
         pass
