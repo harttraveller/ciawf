a18c30d1a702a579d2afff7f3cba8c5a3035e332
Hart Traveller <hart@MBP.local>
2023-06-19 20:44:02 -0400

    ciawf/env: update

--- ciawf/env.py
+++ ciawf/env.py
@@ -1,3 +1,3 @@
-BASE_URL = "https://www.cia.gov/the-world-factbook/"
+BASE_URL = "https://www.cia.gov/the-world-factbook/page-data/countries/page-data.json"
 
 # COUNTRIES_PAGE_DATA_URL = "page-data/countries/page-data.json"
